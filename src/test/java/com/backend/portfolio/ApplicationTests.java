fd7e3b068e6e3d49847fbe212de5a62e2c5b71f1
Luciano-Villani <lucianov@visualdigital.com.ar>
2023-01-14 12:39:38 -0300

    nuevas depen

--- src/test/java/com/backend/portfolio/ApplicationTests.java
+++ src/test/java/com/backend/portfolio/ApplicationTests.java
@@ -1,9 +1,8 @@
 package com.backend.portfolio;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//import org.springframework.boot.test.context.SpringBootTest;
+//@SpringBootTest
 class ApplicationTests {
 
     @Test
